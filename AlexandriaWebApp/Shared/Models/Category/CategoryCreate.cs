14d9c6c0d5107b01803275f23fb18fc44a0f2579
Hunter <hunterjacobibrowning@gmail.com>
2022-04-26 14:48:09 -0400

    Added Category Models to Shared

---
+++ AlexandriaWebApp/Shared/Models/Category/CategoryCreate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlexandriaWebApp.Shared.Models.Category
+{
+    public class CategoryCreate
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ AlexandriaWebApp/Shared/Models/Category/CategoryEdit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlexandriaWebApp.Shared.Models.Category
+{
+    public class CategoryEdit
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
