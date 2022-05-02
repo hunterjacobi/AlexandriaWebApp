5b203ffb5abc6ab544b87510fa96c3ab2051c441
Hunter <hunterjacobibrowning@gmail.com>
2022-05-02 15:54:43 -0400

    Added Index, and Create, Category pages

--- AlexandriaWebApp/Client/Program.cs
+++ AlexandriaWebApp/Client/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AlexandriaWebApp.Client
 {
